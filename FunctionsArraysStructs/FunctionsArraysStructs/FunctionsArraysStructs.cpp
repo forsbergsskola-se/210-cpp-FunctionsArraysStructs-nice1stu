a76fbdec397d71fd70b70377448f332b208768a4
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-08 22:30:51 +0100

    Update FunctionsArraysStructs.cpp
    Number Array Average

--- FunctionsArraysStructs/FunctionsArraysStructs/FunctionsArraysStructs.cpp
+++ FunctionsArraysStructs/FunctionsArraysStructs/FunctionsArraysStructs.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 //Fibonacci
 //fn = fn-1 + fn-2
-
 int fibonacciIterative(int n)
 {
     int i, fnOne = 0, fnTwo = 1, fnThree;
@@ -25,6 +24,20 @@ int fibonacciRecursive(int r)
 {
     if (r <= 1) return r;
     return fibonacciRecursive(r - 1) + fibonacciRecursive(r - 2);
+}
+
+//ArrayAverage
+void getUserInput(int numArray[], int numArraySize)
+{
+    int total = 0;
+    for (size_t i = 0; i < numArraySize; ++i)
+    {
+        cout << "Enter a number & press Enter\n";
+        cin >> numArray[i];
+        total += numArray[i];
+    }
+    cout << "Total: " << total << endl;
+    cout << "Average: " << (total / numArraySize) << endl;
 }
 
 
@@ -82,6 +95,9 @@ int main()
     int r = 3;
     cout << "Recursive fibonacci of " << r << " is " << fibonacciRecursive(r) << endl;
     
+    //ArrayAverage
+    int number[9];
+    getUserInput(number, 9);
 
     return 0;
 }
