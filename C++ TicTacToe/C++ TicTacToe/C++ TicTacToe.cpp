59d4a245b42e2dd371c8118fd0d02302c1f76f85
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-11 07:17:35 +0100

    Update C++ TicTacToe.cpp

--- C++ TicTacToe/C++ TicTacToe/C++ TicTacToe.cpp
+++ C++ TicTacToe/C++ TicTacToe/C++ TicTacToe.cpp
@@ -1,305 +1,114 @@
-//-- TicTacToe --
-
 #include <iostream>
-#include <string>
-#include <thread>
-#include <cmath>
-#include <Windows.h>
-
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-// declare & initialize
-char gameBoard[3][3];
-char playerSymbol[2] = { 'X', 'O' };
-string playerName[2]; // store player names
-int numPlayers;
-int currentPlayer = 1;
-int playerChoiceX = 0;
-int playerChoiceY = 0;
-int BOARD_SIZE = 3;
-bool gameOver = false;
+const int BOARD_SIZE = 9;
+char board[BOARD_SIZE];
 
+void initBoard() {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        board[i] = '-';
+    }
+}
 
-// function prototypes
-void SetUp();
-void LetsPlay();
-void DrawBoard();
-void PlayerTurn();
-void BaymaxTurn();
-void CheckWin();
-void CheckDraw();
+void printBoard() {
+    cout << " " << board[0] << " | " << board[1] << " | " << board[2] << " " << endl;
+    cout << "-----------" << endl;
+    cout << " " << board[3] << " | " << board[4] << " | " << board[5] << " " << endl;
+    cout << "-----------" << endl;
+    cout << " " << board[6] << " | " << board[7] << " | " << board[8] << " " << endl;
+}
 
-// main function
-int main()
-{
-    SetUp();
-    LetsPlay();
+bool isValidMove(int position) {
+    if (position < 0 || position > 8) {
+        return false;
+    }
+    if (board[position] != '-') {
+        return false;
+    }
+    return true;
+}
+
+bool checkWin(char symbol) {
+    // Check rows
+    for (int i = 0; i < 9; i += 3) {
+        if (board[i] == symbol && board[i + 1] == symbol && board[i + 2] == symbol) {
+            return true;
+        }
+    }
+
+    // Check columns
+    for (int i = 0; i < 3; i++) {
+        if (board[i] == symbol && board[i + 3] == symbol && board[i + 6] == symbol) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if (board[0] == symbol && board[4] == symbol && board[8] == symbol) {
+        return true;
+    }
+    if (board[2] == symbol && board[4] == symbol && board[6] == symbol) {
+        return true;
+    }
+
+    return false;
+}
+
+int main() {
+    int player = 1;
+    char symbol = 'X';
+    int position;
+    int moveCount = 0;
+
+    initBoard();
+    printBoard();
+
+    while (true) {
+        cout << "Player " << player << "'s turn (" << symbol << ")" << endl;
+        if (player == 1) {
+            cout << "Enter a position (0-8): ";
+            cin >> position;
+        }
+        else {
+            do {
+                srand(time(NULL)); // seed the random number generator
+                position = rand() % 9; // generate a random number between 0 and 8
+            } while (!isValidMove(position));
+            cout << "Computer chose position " << position << endl;
+        }
+
+        if (!isValidMove(position)) {
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+
+        board[position] = symbol;
+        printBoard();
+        moveCount++;
+
+        if (checkWin(symbol)) {
+            cout << "Player " << player << " wins!" << endl;
+            break;
+        }
+
+        if (moveCount == BOARD_SIZE) {
+            cout << "The game is a draw." << endl;
+            break;
+        }
+
+        if (player == 1) {
+            player = 2;
+            symbol = 'O';
+        }
+        else {
+            player = 1;
+            symbol = 'X';
+        }
+    }
+
     return 0;
 }
 
-void LetsPlay()
-{
-    while (!gameOver)
-    {
-        if (numPlayers == 2)
-        {
-            currentPlayer = (currentPlayer + 1) % 2;
-            PlayerTurn();
-        }
-        else
-        {
-            currentPlayer = (currentPlayer + 1) % 2;
-            if (currentPlayer == 0) PlayerTurn();
-            else BaymaxTurn();
-        }
-        DrawBoard();
-        CheckWin();
-        CheckDraw();
-    }
-}
-
-// Setup Game
-void SetUp()
-{
-    // reset gameBoard array values
-    gameBoard[0][0] = '1'; gameBoard[1][0] = '2'; gameBoard[2][0] = '3';
-    gameBoard[0][1] = '4'; gameBoard[1][1] = '5'; gameBoard[2][1] = '6';
-    gameBoard[0][2] = '7'; gameBoard[1][2] = '8'; gameBoard[2][2] = '9';
-
-    cout << "--X- Welcome to TicTacToe -O--" << endl;
-    cout << "     - by Stewart Wan -" << endl;
-
-    // Number of players
-    int numPlayers;
-    while (true)
-    {
-        cout << "Please enter the number of player 1 or 2" << endl;
-        string userInput;
-        cin >> userInput;
-        switch (stoi(userInput))
-        {
-        case 1:
-            numPlayers = 1;
-            cout << "Player 1, please enter your name" << endl;
-            cin >> playerName[0];
-            playerName[1] = "Baymax";
-            cout << endl << "Lets Play !" << endl << endl;
-            DrawBoard();
-            LetsPlay();
-            return;
-        case 2:
-            numPlayers = 2;
-            for (int i = 0; i < 2; i++)
-            {
-                cout << "Player " << i + 1 << ", please enter your name" << endl;
-                cin >> playerName[i];
-            }
-            cout << endl << "Lets Play !" << endl << endl;
-            DrawBoard();
-            LetsPlay();
-            return;
-        default:
-            cout << "Invalid input." << endl;
-            break;
-        }
-    }
-}
-
-
-void DrawBoard()
-{
-    system("cls"); // Clear the console
-
-    // Set the color of the text to green
-    std::cout << "\033[32m";
-
-    std::cout << std::endl;
-    std::cout << " " << gameBoard[0][2] << " | " << gameBoard[1][2] << " | " << gameBoard[2][2] << std::endl;
-    std::cout << " - + - + -" << std::endl;
-    std::cout << " " << gameBoard[0][1] << " | " << gameBoard[1][1] << " | " << gameBoard[2][1] << std::endl;
-    std::cout << " - + - + - " << std::endl;
-    std::cout << " " << gameBoard[0][0] << " | " << gameBoard[1][0] << " | " << gameBoard[2][0] << std::endl;
-    std::cout << std::endl;
-
-    // Reset the color of the text to the default color
-    std::cout << "\033[0m";
-}
-
-void NumKeyInput()
-{
-    cout << "Player " << playerName[currentPlayer] << ", enter the number of the cell you want to play: " << endl;
-    int playerInput;
-    cin >> playerInput;
-    playerInput--;
-    playerChoiceX = playerInput % BOARD_SIZE;
-    playerChoiceY = playerInput / BOARD_SIZE;
-}
-
-void PlayerTurn()
-{
-PlayerInput:
-    NumKeyInput();
-    if (gameBoard[playerChoiceX][playerChoiceY] == 'X' || gameBoard[playerChoiceX][playerChoiceY] == 'O')
-    {
-        cout << "Cell has been played, please select another number" << endl;
-        goto PlayerInput;
-    }
-    gameBoard[playerChoiceX][playerChoiceY] = playerSymbol[currentPlayer];
-}
-
-void HasWon()
-{
-    DrawBoard();
-    cout << "Player " << playerName[currentPlayer] << " has won!" << endl;
-    gameOver = true;
-}
-
-void CheckWin()
-{
-    // check rows
-    for (int i = 0; i < 3; i++) {
-        if (gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][1] == gameBoard[i][2])
-        {
-            HasWon();
-            return;
-        }
-    }
-
-    // check columns
-    for (int i = 0; i < 3; i++) {
-        if (gameBoard[0][i] == gameBoard[1][i] && gameBoard[1][i] == gameBoard[2][i])
-        {
-            HasWon();
-            return;
-        }
-    }
-
-    // check diagonals
-    if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2]) {
-        HasWon();
-        return;
-    }
-    if (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0]) {
-        HasWon();
-        return;
-    }
-}
-
-void CheckDraw() {
-    bool emptyFound = false;
-    for (int i = 0; i < BOARD_SIZE; i++)
-    {
-        for (int j = 0; j < BOARD_SIZE; j++)
-        {
-            if (gameBoard[i][j] != 'X' && gameBoard[i][j] != 'O')
-            {
-                emptyFound = true;
-                break;
-            }
-        }
-    }
-    if (!emptyFound)
-    {
-        DrawBoard();
-        cout << "The game is a draw." << endl;
-        gameOver = true;
-    }
-}
-
-//End Game
-void EndGame()
-{
-    cout << "Thanks for Playing" << endl;
-    exit(0);
-}
-
-void PlayAgain()
-{
-    while (true)
-    {
-        cout << "Would you like to play again?" << endl;
-        cout << "[1] for YES : [2] for NO" << endl;
-
-        string userInput;
-        getline(cin, userInput);
-
-        if (userInput != "1" && userInput != "2")
-        {
-            cout << "Invalid input." << endl;
-        }
-        else
-        {
-            int playAgain = stoi(userInput);
-            if (playAgain == 1)
-            {
-                system("cls");
-                SetUp();
-            }
-            else
-            {
-                system("cls");
-                EndGame();
-            }
-            break;
-        }
-    }
-}
-
-
-// Single Player Methods
-// Generate a random number between 0 and max-1
-int GenerateRandomNumber(int max)
-{
-    srand(time(NULL));
-    return rand() % max;
-}
-
-// Get a valid move from the player
-void GetPlayerMove()
-{
-    while (true)
-    {
-        // Prompt the player for input
-        cout << "Enter your move (1-9): ";
-        string input;
-        getline(cin, input);
-
-        // Validate the input
-        if (input.size() != 1 || input[0] < '1' || input[0] > '9')
-        {
-            cout << "Invalid input. Please enter a number between 1 and 9." << endl;
-            continue;
-        }
-
-        // Convert the input to coordinates on the game board
-        int position = input[0] - '1';
-        playerChoiceX = position % BOARD_SIZE;
-        playerChoiceY = position / BOARD_SIZE;
-
-        // Check if the chosen position is available
-        if (gameBoard[playerChoiceX][playerChoiceY] != ' ')
-        {
-            cout << "That position is already occupied. Please choose a different position." << endl;
-            continue;
-        }
-
-        // The move is valid, so exit the loop
-        break;
-    }
-}
-
-// Let Baymax make a move
-void BaymaxTurn()
-{
-    int x, y;
-    do
-    {
-        x = GenerateRandomNumber(BOARD_SIZE);
-        y = GenerateRandomNumber(BOARD_SIZE);
-    } while (gameBoard[x][y] != ' ');
-
-    gameBoard[x][y] = playerSymbol[1];
-    cout << "Baymax plays " << gameBoard[x][y] << endl;
-    Sleep(2000);
-}
