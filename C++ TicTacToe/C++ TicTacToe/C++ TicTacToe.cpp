337200f4567eff1525da8309089c6f204b7ae986
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-12 06:34:29 +0100

    Update C++ TicTacToe.cpp
    got the board printing the array index... need to fix it to represnt the numpad if possible

--- C++ TicTacToe/C++ TicTacToe/C++ TicTacToe.cpp
+++ C++ TicTacToe/C++ TicTacToe/C++ TicTacToe.cpp
@@ -15,11 +15,17 @@ void initBoard()
 
 void printBoard()
 {
-    cout << " " << board[0] << " | " << board[1] << " | " << board[2] << " " << endl;
+    cout << " " << (board[0] == '-' ? "0" : string(1, board[0])) << " | "
+        << (board[1] == '-' ? "1" : string(1, board[1])) << " | "
+        << (board[2] == '-' ? "2" : string(1, board[2])) << " " << endl;
     cout << "---+---+---" << endl;
-    cout << " " << board[3] << " | " << board[4] << " | " << board[5] << " " << endl;
+    cout << " " << (board[3] == '-' ? "3" : string(1, board[3])) << " | "
+        << (board[4] == '-' ? "4" : string(1, board[4])) << " | "
+        << (board[5] == '-' ? "5" : string(1, board[5])) << " " << endl;
     cout << "---+---+---" << endl;
-    cout << " " << board[6] << " | " << board[7] << " | " << board[8] << " " << endl;
+    cout << " " << (board[6] == '-' ? "6" : string(1, board[6])) << " | "
+        << (board[7] == '-' ? "7" : string(1, board[7])) << " | "
+        << (board[8] == '-' ? "8" : string(1, board[8])) << " " << endl;
 }
 
 bool isValidMove(int position)
@@ -33,37 +39,64 @@ bool checkWin(int position, char symbol)
     int col = position % 3;
     bool isDiagonal = (row == col) || (row + col == 2);
 
-    bool hasWon = true;
+    bool hasWon = false;
     for (int i = row * 3; i < row * 3 + 3; i++)
     {
-        return (board[i] != symbol);
+        if (board[i] != symbol)
+        {
+            hasWon = false;
+            break;
+        }
+        hasWon = true;
     }
     if (hasWon) return true;
 
-    hasWon = true;
+    hasWon = false;
     for (int i = col; i < BOARD_SIZE; i += 3)
     {
-        hasWon = (board[i] == symbol) ? true : false;
+        if (board[i] != symbol)
+        {
+            hasWon = false;
+            break;
+        }
+        hasWon = true;
     }
     if (hasWon) return true;
 
     if (isDiagonal)
     {
-        hasWon = true;
+        hasWon = false;
         if (row == col && board[0] == symbol && board[4] == symbol && board[8] == symbol)
         {
             hasWon = true;
         }
         else
         {
-            for (int i = 2; i < BOARD_SIZE - 1; i += 2)
+            for (int i = 2; i <= 6; i += 2)
             {
-                hasWon = (board[i] == symbol);
+                if (board[i] != symbol)
+                {
+                    hasWon = false;
+                    break;
+                }
+                hasWon = true;
             }
         }
         if (hasWon) return true;
     }
     return false;
+}
+
+int getPlayerMove(char symbol)
+{
+    int position;
+    do
+    {
+        cout << "Enter a position (0-8): ";
+        cin >> position;
+    } while (!isValidMove(position));
+    board[position] = symbol;
+    return position;
 }
 
 int main()
@@ -79,56 +112,94 @@ int main()
     while (true)
     {
         cout << "Player " << player << "'s turn (" << symbol << ")" << endl;
+        int position;
+
         if (player == 1)
         {
-            cout << "Enter a position (0-8): ";
-            cin >> position;
+            // Player's turn
+            do
+            {
+                cout << "Enter a position (0-8): ";
+                cin >> position;
+            }
+            while (!isValidMove(position));
         }
         else
         {
-            do
+            // Computer's turn
+            bool hasMoved = false;
+
+            // Check for a winning move
+            for (int i = 0; i < BOARD_SIZE; i++)
             {
-                srand(time(NULL));
-                position = rand() % 9;
+                if (isValidMove(i))
+                {
+                    board[i] = symbol;
+                    if (checkWin(i, symbol))
+                    {
+                        position = i;
+                        hasMoved = true;
+                        break;
+                    }
+                    board[i] = '-';
+                }
             }
-            while (!isValidMove(position));
+
+            // Check for a blocking move
+            if (!hasMoved)
+            {
+                char opponentSymbol = (symbol == 'X') ? 'O' : 'X';
+                for (int i = 0; i < BOARD_SIZE; i++)
+                {
+                    if (isValidMove(i))
+                    {
+                        board[i] = opponentSymbol;
+                        if (checkWin(i, opponentSymbol))
+                        {
+                            position = i;
+                            hasMoved = true;
+                            break;
+                        }
+                        board[i] = '-';
+                    }
+                }
+            }
+
+            // Choose a random move if no winning or blocking move is possible
+            if (!hasMoved)
+            {
+                do
+                {
+                    srand(time(NULL));
+                    position = rand() % 9;
+                }
+                while (!isValidMove(position));
+            }
+
             cout << "Computer chose position " << position << endl;
         }
 
-        if (!isValidMove(position))
-        {
-            cout << "Invalid move. Try again." << endl;
-            continue;
-        }
-
+        // Make the move
         board[position] = symbol;
         printBoard();
-        moveCount++;
 
+        // Check if the game is over
         if (checkWin(position, symbol))
         {
             cout << "Player " << player << " wins!" << endl;
             break;
         }
-
-
-        if (moveCount == BOARD_SIZE)
+        else if (moveCount == BOARD_SIZE - 1)
         {
             cout << "The game is a draw." << endl;
             break;
         }
 
-        if (player == 1)
-        {
-            player = 2;
-            symbol = 'O';
-        }
-        else
-        {
-            player = 1;
-            symbol = 'X';
-        }
+        // Switch to the other player
+        player = (player == 1) ? 2 : 1;
+        symbol = (symbol == 'X') ? 'O' : 'X';
+        moveCount++;
     }
+
     return 0;
 }
-
