f8f36fe4d7868bce4f8a73570fe4b3eb4bfd7599
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-12 22:42:26 +0100

    working
    Got it working

--- FunctionsArraysStructs/PlaindromeChecker/PlaindromeChecker.cpp
+++ FunctionsArraysStructs/PlaindromeChecker/PlaindromeChecker.cpp
@@ -1,20 +1,32 @@
-// PlaindromeChecker.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+bool isPalindrome(string userInput)
+{
+    int n = userInput.length();
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (userInput[i] != userInput[n - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char* userInput = new char[32];
+
+    cout << "enter a word to check then press [Enter]" << endl;
+    cin.getline(userInput, 32);
+
+    if (isPalindrome(userInput)) cout << "It is a palindrome" << endl;
+    
+    else cout << "It is not a palindrome" << endl;
+
+    delete[] userInput;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
