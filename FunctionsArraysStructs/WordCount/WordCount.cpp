214982bcfe3bca440c2aa4ff5eca80a3274af583
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-12 21:00:47 +0100

    Update WordCount.cpp
    can't get the user input to work, it hangs so try with set message and it seems to work

--- FunctionsArraysStructs/WordCount/WordCount.cpp
+++ FunctionsArraysStructs/WordCount/WordCount.cpp
@@ -1,15 +1,34 @@
-#include <cstdio>
 #include <iostream>
 #include <vector>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-	vector<char> userInput;
-	char c;
-	int wordCount = 0;
-	bool isWord = false;
+    vector<char> userInput = { 'H', 'e', 'l', 'l', 'o', ',', ' ', 'm', 'y', ' ', 'n', 'a', 'm', 'e', ' ', 'i', 's', ' ', 'M', 'a', 'r', 'c' };
+    int wordCount = 0;
+    bool isWord = false;
 
-	cout << "Please type in a sentence. Press [Enter] to finish." << endl;
+    for (auto c : userInput)
+    {
+        if (isalpha(c))
+        {
+            isWord = true;
+        }
+        else if (isWord)
+        {
+            wordCount++;
+            isWord = false;
+        }
+    }
+
+    if (isWord)
+    {
+        wordCount++;
+    }
+
+    cout << "The sentence has " << wordCount << " words." << endl;
+
+    return 0;
 }
