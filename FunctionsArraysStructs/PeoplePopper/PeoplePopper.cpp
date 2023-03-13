ddce6f6fdc3995bdb8d8cd29d6c7f9490f69c73e
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-13 20:22:06 +0100

    Set up people vector
    using vector to create a dynamic array

--- FunctionsArraysStructs/PeoplePopper/PeoplePopper.cpp
+++ FunctionsArraysStructs/PeoplePopper/PeoplePopper.cpp
@@ -1,20 +1,30 @@
-// PeoplePopper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+
+int howMany = 0;
+
+using namespace std;
+
+vector<string> PeopleArray;
+
+//People struct
+struct Person
+{
+    string personName;
+};
+
+void FillInPeopleArray(vector<Person>& arr)
+{
+    for (size_t i = 0; i < arr.size(); ++i)
+    {
+        cout << "What's the next person supposed to be called?" << i + 1 << endl;
+        cin >> arr[i].personName;
+        cout << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "How many people would you like to create?" << endl;
+    cin >> howMany;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
