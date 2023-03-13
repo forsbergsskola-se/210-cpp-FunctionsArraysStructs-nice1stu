d6fb835695289ad7b95131ce3e799116ee765e18
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-13 20:42:20 +0100

    Update PeoplePopper.cpp
    crated vector to take user input for number of people, input names and print names with comma.... but not pointer.....

--- FunctionsArraysStructs/PeoplePopper/PeoplePopper.cpp
+++ FunctionsArraysStructs/PeoplePopper/PeoplePopper.cpp
@@ -1,30 +1,46 @@
 #include <iostream>
 #include <vector>
 
-int howMany = 0;
-
 using namespace std;
 
 vector<string> PeopleArray;
+int howMany = 0;
 
-//People struct
 struct Person
 {
     string personName;
 };
 
-void FillInPeopleArray(vector<Person>& arr)
+void FillInPeopleArray(vector<Person>& people)
 {
-    for (size_t i = 0; i < arr.size(); ++i)
+    for (size_t i = 0; i < people.size(); ++i)
     {
         cout << "What's the next person supposed to be called?" << i + 1 << endl;
-        cin >> arr[i].personName;
+        cin >> people[i].personName;
         cout << endl;
     }
 }
+
+void DisplayPeopleArrayNames(const vector<Person>& people)
+{
+    for (size_t i = 0; i < people.size(); ++i)
+    {
+        cout << people[i].personName;
+        if (i != people.size() - 1)
+        {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+}
+
 
 int main()
 {
     cout << "How many people would you like to create?" << endl;
     cin >> howMany;
+
+    vector<Person> PeopleArray(howMany);
+    FillInPeopleArray(PeopleArray);
+    DisplayPeopleArrayNames(PeopleArray);
 }
