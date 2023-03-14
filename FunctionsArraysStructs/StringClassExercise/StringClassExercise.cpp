17d8c0175193466948266f4406fed74578da9bc7
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-15 00:27:20 +0100

    Update StringClassExercise.cpp

--- FunctionsArraysStructs/StringClassExercise/StringClassExercise.cpp
+++ FunctionsArraysStructs/StringClassExercise/StringClassExercise.cpp
@@ -33,10 +33,8 @@ public:
     void append(const char* text)
     {
         size_t textLength = strlen(text);
-        if (length + textLength >= maxSize)
-        {
-            throw runtime_error("String would exceed max size!");
-        }
+        if (length + textLength >= maxSize) throw runtime_error("String would exceed max size!");
+        
         buffer.resize(length + textLength);
         for (int i = 0; i < textLength; i++)
         {
@@ -50,10 +48,8 @@ public:
     void appendLine(const char* text)
     {
         size_t textLength = char_traits<char>::length(text);
-        if (this->length + textLength + 1 >= this->maxSize)
-        {
-            throw runtime_error("String with line break would exceed max size!");
-        }
+        if (this->length + textLength + 1 >= this->maxSize) throw runtime_error("String with line break would exceed max size!");
+
         this->buffer += text;
         this->buffer += '\n';
         this->length += textLength + 1;
@@ -75,11 +71,27 @@ public:
 
 int main()
 {
-    StringClassExercise myString(20);
-    myString.append("Hello ");
-    myString.appendLine("world!");
-    myString.print();
-    const char* bufferString = myString.getString();
-    std::cout << "C-style string: " << bufferString << std::endl;
+    try {
+        StringClassExercise s1(20);
+        s1.append("Foo");
+        s1.appendLine("Bar");
+        cout << "s1: ";
+        s1.print();
+
+        StringClassExercise s2("This is a long string", 25);
+        s2.append(" and this is more");
+        cout << "s2: " << s2.getString() << endl;
+
+        StringClassExercise s3(5);
+        s3.append("test");
+        s3.appendLine("longer text");
+        cout << "s3: " << s3.getString() << endl;
+    }
+    catch (const exception& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+
     return 0;
 }
+
