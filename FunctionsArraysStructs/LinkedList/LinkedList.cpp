d3dc41224d84e38b4b5cc71994ef6027101ebf3f
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-14 21:13:14 +0100

    Create new LinkList project with test

--- FunctionsArraysStructs/LinkedList/LinkedList.cpp
+++ FunctionsArraysStructs/LinkedList/LinkedList.cpp
@@ -1,9 +1,106 @@
-// LinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//C++ Linked List
 #include <iostream>
 
-int main()
+using namespace std;
+
+template <typename ItemType>
+class LinkedList
 {
-    std::cout << "Hello World!\n";
+public:
+    LinkedList();
+    ~LinkedList();
+
+    void Add(const ItemType& item);
+    void Remove(const ItemType& item);
+    uint64_t size();
+
+private:
+    struct Node
+    {
+        Node* next;
+        Node* previous;
+        ItemType data;
+    };
+
+    Node* head;
+    Node* tail;
+
+    Node* makeNode(const ItemType& item);
+};
+
+template <typename ItemType>
+LinkedList<ItemType>::LinkedList()
+{
+    head = nullptr;
+    tail = nullptr;
 }
+
+template <typename ItemType>
+LinkedList<ItemType>::~LinkedList()
+{
+    Node* current = head;
+    while (current != nullptr)
+    {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
+}
+
+template <typename ItemType>
+typename LinkedList<ItemType>::Node* LinkedList<ItemType>::makeNode(const ItemType& item)
+{
+    Node* node = new Node;
+    node->data = item;
+    node->next = nullptr;
+    node->previous = nullptr;
+    return node;
+}
+
+template <typename ItemType>
+void LinkedList<ItemType>::Add(const ItemType& item)
+{
+    Node* node = makeNode(item);
+
+    if (head == nullptr)
+    {
+        head = node;
+        tail = node;
+    }
+    else
+    {
+        tail->next = node;
+        node->previous = tail;
+        tail = node;
+    }
+}
+
+template <typename ItemType>
+void LinkedList<ItemType>::Remove(const ItemType& item)
+{
+    // TODO: Implement Remove function
+}
+
+template <typename ItemType>
+uint64_t LinkedList<ItemType>::size()
+{
+    uint64_t count = 0;
+    Node* current = head;
+    while (current != nullptr)
+    {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+
+// node
+// count
+// AddRange
+// Add
+// Get
+// Set
+// Remove
+// Clear
+// IEnumeraotr?
--- FunctionsArraysStructs/LinkedListTest/LinkedListTest.cpp
+++ FunctionsArraysStructs/LinkedListTest/LinkedListTest.cpp
@@ -6,13 +6,29 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LinkedListTest
 {
-	TEST_CLASS(LinkedListTest)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			LinkedList<int> linkedList;
-		}
-	};
+    TEST_CLASS(UnitTestLinkedList)
+    {
+    public:
+        TEST_METHOD(TestAddOne)
+        {
+            LinkedList<int> list;
+            list.Add(1);
+            Assert::AreEqual(1, list.Size(), L"Size of list should be 1", LINE_INFO());
+        }
+
+        TEST_METHOD(TestEmptyList)
+        {
+            LinkedList<int> list;
+            Assert::AreEqual(0, list.Size(), L"Size of list should be 0", LINE_INFO());
+        }
+    };
 }
+
+// Setup
+// TestGetValueFromCorrectIndex()
+// TestSetIteminCorrectIndex()
+// TestCountIsZeroAfterClear()
+// TestRemoveIndexAt()
+// TestContainsValueAtCorrectIndex()
+// TestIndexOf()
+// TestRemove()
