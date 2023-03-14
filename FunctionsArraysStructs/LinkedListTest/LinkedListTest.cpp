7bf0a9d04cd46ce19c39ecf1d3960d8158dc7937
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-14 14:23:18 +0100

    create new linkedlist and test proj

---
+++ FunctionsArraysStructs/LinkedListTest/LinkedListTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../C++LinkedList/C++LinkedList.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace LinkedListTest
+{
+	TEST_CLASS(LinkedListTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			LinkedList linkedList;
+		}
+	};
+}
