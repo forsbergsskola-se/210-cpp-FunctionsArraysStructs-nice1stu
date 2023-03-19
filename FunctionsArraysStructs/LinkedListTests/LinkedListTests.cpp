6e0c2f31f6c96e7587576623193a96eebec16be4
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-19 22:43:21 +0100

    Update LinkedListTests.cpp
    change  MultiAdd test to use std list

--- FunctionsArraysStructs/LinkedListTests/LinkedListTests.cpp
+++ FunctionsArraysStructs/LinkedListTests/LinkedListTests.cpp
@@ -54,45 +54,15 @@ namespace LinkedListTest
 
         TEST_METHOD(TestEmptyList)
         {
-            list.Clear();
+            ClearStdList();
             Assert::AreEqual(list.Count(), 0u, L"Size of list should be 0", LINE_INFO());
         }
 
         TEST_METHOD(TestMultiAdd)
         {
-            LinkedList<int> list;
-            vector<int> items{ 1, 2, 3 };
+            vector<int> items{ 0, -2, 54 };
             list.MultiAdd(items);
-            Assert::AreEqual(list.Count(), 3u, L"Size of list should be 3", LINE_INFO());
-        }
-
-        TEST_METHOD(TestGetItemFromCorrectIndex)
-        {
-            LinkedList<int> list;
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            Assert::AreEqual(list.Get(1), 2, L"Item at index 1 should be 2", LINE_INFO());
-        }
-
-        TEST_METHOD(TestSetIteminCorrectIndex)
-        {
-            LinkedList<int> list;
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Set(1, 4);
-            Assert::AreEqual(list.Get(1), 4, L"Item at index 1 should be 4", LINE_INFO());
-        }
-
-        TEST_METHOD(TestSetIteminCorrectIndex)
-        {
-            LinkedList<int> list;
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Set(1, 4);
-            Assert::AreEqual(list.Get(1), 4, L"Item at index 1 should be 4", LINE_INFO());
+            Assert::AreEqual(list.Count(), 8u, L"Size of list should be 8", LINE_INFO());
         }
     };
 }
