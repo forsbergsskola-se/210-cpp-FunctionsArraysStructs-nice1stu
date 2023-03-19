342c56d840d8b7c624cddbf21f154963b1394690
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-19 22:02:15 +0100

    Update LinkedListTests.cpp
    Add tests multiadd and get item from correct inden

--- FunctionsArraysStructs/LinkedListTests/LinkedListTests.cpp
+++ FunctionsArraysStructs/LinkedListTests/LinkedListTests.cpp
@@ -1,3 +1,12 @@
+// Setup
+// TestGetValueFromCorrectIndex()
+// TestSetIteminCorrectIndex()
+// TestCountIsZeroAfterClear()
+// TestRemoveIndexAt()
+// TestContainsValueAtCorrectIndex()
+// TestIndexOf()
+// TestRemove()
+
 #include "pch.h"
 #include "CppUnitTest.h"
 #include "../LinkedList/LinkedList.cpp"
@@ -13,22 +22,32 @@ namespace LinkedListTest
         {
             LinkedList<int> list;
             list.Add(1);
-            Assert::AreEqual(1, list.Size(), L"Size of list should be 1", LINE_INFO());
+            Assert::AreEqual(list.Count(), 1u, L"Size of list should be 1", LINE_INFO());
         }
 
         TEST_METHOD(TestEmptyList)
         {
             LinkedList<int> list;
-            Assert::AreEqual(0, list.Size(), L"Size of list should be 0", LINE_INFO());
+            Assert::AreEqual(list.Count(), 0u, L"Size of list should be 0", LINE_INFO());
         }
+
+        TEST_METHOD(TestMultiAdd)
+        {
+            LinkedList<int> list;
+            vector<int> items{ 1, 2, 3 };
+            list.MultiAdd(items);
+            Assert::AreEqual(list.Count(), 3u, L"Size of list should be 3", LINE_INFO());
+        }
+
+        TEST_METHOD(TestGetItemFromCorrectIndex)
+        {
+            LinkedList<int> list;
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Assert::AreEqual(list.Get(1), 2, L"Item at index 1 should be 2", LINE_INFO());
+        }
+
+
     };
 }
-
-// Setup
-// TestGetValueFromCorrectIndex()
-// TestSetIteminCorrectIndex()
-// TestCountIsZeroAfterClear()
-// TestRemoveIndexAt()
-// TestContainsValueAtCorrectIndex()
-// TestIndexOf()
-// TestRemove()
