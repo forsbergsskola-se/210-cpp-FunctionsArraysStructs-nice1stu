471be510e3ccedd7ef6476641d2484043b317831
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-03-24 03:09:47 +0100

    Update C++ TicTacToe.cpp
    trying to refactor to introduce function

--- C++ TicTacToe/C++ TicTacToe/C++ TicTacToe.cpp
+++ C++ TicTacToe/C++ TicTacToe/C++ TicTacToe.cpp
@@ -8,73 +8,19 @@ using namespace std;
 const int BOARD_SIZE = 9;
 char board[BOARD_SIZE];
 
-enum class BoardValue {
+enum class BoardValue
+{
     Empty = '-',
     Player1 = 'X',
     Player2 = 'O',
 };
 
-void initBoard() {
-    fill(begin(board), end(board), static_cast<char>(BoardValue::Empty));
-}
+void initBoard();
+void printBoard();  // forward declaration
 
-void printBoard() {
-    auto printCell = [](BoardValue value) {
-        switch (value) {
-        case BoardValue::Empty: return "\033[31m-\033[0m";
-        case BoardValue::Player1: return "X";
-        case BoardValue::Player2: return "O";
-        }
-    };
-
-    cout << " " << printCell(static_cast<BoardValue>(board[0])) << " | "
-        << printCell(static_cast<BoardValue>(board[1])) << " | "
-        << printCell(static_cast<BoardValue>(board[2])) << " " << endl;
-    cout << " - + - + -" << endl;
-    cout << " " << printCell(static_cast<BoardValue>(board[3])) << " | "
-        << printCell(static_cast<BoardValue>(board[4])) << " | "
-        << printCell(static_cast<BoardValue>(board[5])) << " " << endl;
-    cout << " - + - + -" << endl;
-    cout << " " << printCell(static_cast<BoardValue>(board[6])) << " | "
-        << printCell(static_cast<BoardValue>(board[7])) << " | "
-        << printCell(static_cast<BoardValue>(board[8])) << " " << endl;
-}
-
-
-bool isValidMove(int position)
-{
-    return (position >= 0 && position <= 8 && board[position] == '-');
-}
-
-bool checkWin(int position, char symbol)
-{
-    int row = position / 3;
-    int col = position % 3;
-    bool isDiagonal = (row == col) || (row + col == 2);
-
-    bool rowWin = board[row * 3] == symbol && board[row * 3 + 1] == symbol && board[row * 3 + 2] == symbol;
-    bool colWin = board[col] == symbol && board[col + 3] == symbol && board[col + 6] == symbol;
-    bool diagWin = (board[0] == symbol && board[4] == symbol && board[8] == symbol) || (board[2] == symbol && board[4] == symbol && board[6] == symbol);
-
-    return rowWin || colWin || diagWin;
-}
-
-int getPlayerMove(char symbol)
-{
-    int position;
-    while (true)
-    {
-        cout << "Enter a position (1-9): ";
-        cin >> position;
-        position--;
-        if (isValidMove(position)) break;
-        cout << "Invalid move, please try again." << endl;
-    }
-    ++position;
-    board[position - 1] = symbol;
-    cout << "Cell " << position << " now contains " << symbol << endl;
-    return position;
-}
+bool isValidMove(int position);
+bool checkWin(int position, char symbol);
+int getPlayerMove(char symbol);
 
 int main()
 {
@@ -184,4 +130,70 @@ int main()
         ++moveCount;
     }
     return 0;
+}
+
+void initBoard()
+{
+    fill(begin(board), end(board), static_cast<char>(BoardValue::Empty));
+}
+
+void printBoard()
+{
+    auto printCell = [](BoardValue value)
+    {
+        switch (value)
+        {
+        case BoardValue::Empty: return "\033[31m-\033[0m";
+        case BoardValue::Player1: return "X";
+        case BoardValue::Player2: return "O";
+        }
+    };
+
+    cout << " " << printCell(static_cast<BoardValue>(board[0])) << " | "
+        << printCell(static_cast<BoardValue>(board[1])) << " | "
+        << printCell(static_cast<BoardValue>(board[2])) << " " << endl;
+    cout << " - + - + -" << endl;
+    cout << " " << printCell(static_cast<BoardValue>(board[3])) << " | "
+        << printCell(static_cast<BoardValue>(board[4])) << " | "
+        << printCell(static_cast<BoardValue>(board[5])) << " " << endl;
+    cout << " - + - + -" << endl;
+    cout << " " << printCell(static_cast<BoardValue>(board[6])) << " | "
+        << printCell(static_cast<BoardValue>(board[7])) << " | "
+        << printCell(static_cast<BoardValue>(board[8])) << " " << endl;
+}
+
+
+bool isValidMove(int position)
+{
+    return (position >= 0 && position <= 8 && board[position] == '-');
+}
+
+bool checkWin(int position, char symbol)
+{
+    int row = position / 3;
+    int col = position % 3;
+    bool isDiagonal = (row == col) || (row + col == 2);
+
+    bool rowWin = board[row * 3] == symbol && board[row * 3 + 1] == symbol && board[row * 3 + 2] == symbol;
+    bool colWin = board[col] == symbol && board[col + 3] == symbol && board[col + 6] == symbol;
+    bool diagWin = (board[0] == symbol && board[4] == symbol && board[8] == symbol) || (board[2] == symbol && board[4] == symbol && board[6] == symbol);
+
+    return rowWin || colWin || diagWin;
+}
+
+int getPlayerMove(char symbol)
+{
+    int position;
+    while (true)
+    {
+        cout << "Enter a position (1-9): ";
+        cin >> position;
+        position--;
+        if (isValidMove(position)) break;
+        cout << "Invalid move, please try again." << endl;
+    }
+    ++position;
+    board[position - 1] = symbol;
+    cout << "Cell " << position << " now contains " << symbol << endl;
+    return position;
 }
